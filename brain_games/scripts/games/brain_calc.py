57938202a78d784d86a95460b5b1dae3b6ea59ac
Ivanov Egor <ghora0907@gmail.com>
2024-08-04 20:46:35 +0300

    Tweaks 5

--- brain_games/scripts/games/brain_calc.py
+++ brain_games/scripts/games/brain_calc.py
@@ -18,20 +18,16 @@ def main():
     while counter < 3:
        number_1 = random.randint(1, 100)
        number_2 = random.randint(1, 100)
-       random_operation = random.choice((multiply, substract, sum, divide))
+       random_operation = random.choice((multiply, substract, sum, d))
        if random_operation == multiply:
           print(f"Question: {number_1} * {number_2} ")
           answer = number_1 * number_2
        elif random_operation == substract:
           print(f'Question: {number_1} - {number_2} ')
           answer = number_1 - number_2
-       elif random_operation == sum:
+       else random_operation == sum:
           print(f'Question: {number_1} + {number_2} ')
           answer = number_1 + number_2
-       else:
-          print(f'Question: {number_1} // {number_2}')
-          answer = number_1 // number_2
-       user_answer = input("Your answer: ")
        if user_answer.isalpha():
            print("Please, insert a number")
            break
