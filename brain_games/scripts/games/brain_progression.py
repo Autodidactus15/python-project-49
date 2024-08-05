b4264c5e8b35c06ef9fbf10ec4e4504b90d92d7a
Ivanov Egor <ghora0907@gmail.com>
2024-08-05 19:45:41 +0300

    Linting 3

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -1,6 +1,7 @@
 import prompt
 import random
 
+
 def progression(number_1, number_2):
     length = 10
     progression_list = []
@@ -10,30 +11,32 @@ def progression(number_1, number_2):
         number_1 += number_2
     return progression_list
 
+
 def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print("What number is missing in the progression?")
-    
+
     counter = 0
     while counter < 3:
         number_1 = random.randint(1, 100)
         number_2 = random.randint(1, 5)
         number_3 = random.randint(0, 9)
-        
+
         pr_list = progression(number_1, number_2)
         old_pr_list = pr_list.copy()
         answer = old_pr_list[number_3]
         pr_list[number_3] = ".."
-        
-        print(f'Question: {pr_list[0]} {pr_list[1]} {pr_list[2]} {pr_list[3]} {pr_list[4]} {pr_list[5]} {pr_list[6]} {pr_list[7]} {pr_list[8]} {pr_list[9]}')
-        
+
+        print(f'Question: {pr_list[0]} {pr_list[1]} {pr_list[2]} {pr_list[3]} {pr_list[4]} '
+              f'{pr_list[5]} {pr_list[6]} {pr_list[7]} {pr_list[8]} {pr_list[9]}')
+
         user_answer = input("Your answer: ")
         if user_answer.isalpha():
             print("Please, insert a number")
             break
-        
+
         user_answer = int(user_answer)
         if user_answer == int(answer):
             print("Correct")
@@ -45,6 +48,6 @@ def main():
     else:
         print(f'Congratulations, {name}!')
 
+
 if __name__ == '__main__':
     main()
-
