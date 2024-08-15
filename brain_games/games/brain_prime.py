f5a5886af19af3c6312edcd26cb80d971d129f64
Ivanov Egor <ghora0907@gmail.com>
2024-08-15 21:58:45 +0300

    Tweaks 4.0

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -7,7 +7,7 @@ def say_hello():
     global name
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
 
 
 def is_prime(n):
